089aec074238ebd2bf078ca39a12bd56bae04d33
Rahul <rahtiwari@gmail.com>
2020-02-21 19:59:06 +0530

    commiting @qualifier

--- SpringAutowire/src/com/sleniumexpress/Heart.java
+++ SpringAutowire/src/com/sleniumexpress/Heart.java
@@ -1,9 +1,27 @@
 package com.sleniumexpress;
 
 public class Heart {
-	
-	public void pump()
-	{
+
+	private String animalName;
+	private int noOfHearts;
+
+	public String getAnimalName() {
+		return animalName;
+	}
+
+	public void setAnimalName(String animalName) {
+		this.animalName = animalName;
+	}
+
+	public int getNoOfHearts() {
+		return noOfHearts;
+	}
+
+	public void setNoOfHearts(int noOfHearts) {
+		this.noOfHearts = noOfHearts;
+	}
+
+	public void pump() {
 		System.out.println("Pumping is working fine");
 	}
 
--- SpringAutowire/src/com/sleniumexpress/Human.java
+++ SpringAutowire/src/com/sleniumexpress/Human.java
@@ -1,11 +1,26 @@
 package com.sleniumexpress;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class Human {
 	
-	Heart heart;
+	public  Human() {
+		
+	}
 
+	Heart heart;
+	
+	
+	public Human(Heart heart) {
+		this.heart = heart;
+	}
+	@Autowired
+	@Qualifier("hearthuman")
 	public void setHeart(Heart heart) {
 		this.heart = heart;
+		System.out.println("setter method called for heart");
+		System.out.println(heart.getAnimalName() + heart.getNoOfHearts());
 	}
 	
 	public void startPumping()
